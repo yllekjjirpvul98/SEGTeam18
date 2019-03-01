5828657144d892b1dbe9ea6b70991ca971d8a4c8
yllekjjirpvul98 <kyungchingman@gmail.com>
2019-03-01 22:07:24 +0000

    Update database

--- AdAuction/src/Model/Database.java
+++ AdAuction/src/Model/Database.java
@@ -1,9 +1,30 @@
 package Model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 public class Database {
+    private Connection connect = null;
+    private Statement statement = null;
 
-    public void createDatabase() {
-
+    public void connectToDatabase(){
         //method to create database AdAuction using sql
+        try {
+            connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/adauction?" + "user=root&password=Yy26535980");
+            statement = connect.createStatement();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public Statement getStatement(){
+        return statement;
+    }
+
+
+    public static void main(String[] args){
+        Database db = new Database();
+        db.connectToDatabase();
     }
 }
