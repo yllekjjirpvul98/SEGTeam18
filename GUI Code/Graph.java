a957a54562048e53025fdee8c16b7fa0ccba43f1
GregEvers <greg@klassic.me>
2019-03-02 18:24:26 +0000

    A few graph changes

--- GUI Code/Graph.java
+++ GUI Code/Graph.java
@@ -1,15 +1,15 @@
 import javax.swing.*;
-
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.data.category.DefaultCategoryDataset;
-
 import java.awt.*;
+import java.util.HashMap;
 
 public class Graph extends JPanel {
 
-    private String yAxisVar;
+    private String yAxisVar = "Impressions";
+    private HashMap<Double, String> currentGraphData = new HashMap<>();
 
     public Graph() {
 
@@ -19,7 +19,7 @@ public class Graph extends JPanel {
         JFreeChart chart = ChartFactory.createLineChart(
                 "", // Chart title
                 "Date", // X-Axis Label
-                "Number of Impressions", // Y-Axis Label
+                yAxisVar, // Y-Axis Label
                 dataset
         );
 
@@ -31,21 +31,33 @@ public class Graph extends JPanel {
 
     private DefaultCategoryDataset createDataset() {
 
-        yAxisVar = "Impressions";
         String series1 = yAxisVar;
 
         DefaultCategoryDataset dataset = new DefaultCategoryDataset();
 
-        dataset.addValue(200, series1, "2016-12-19");
-        dataset.addValue(150, series1, "2016-12-20");
-        dataset.addValue(100, series1, "2016-12-21");
-        dataset.addValue(210, series1, "2016-12-22");
-        dataset.addValue(240, series1, "2016-12-23");
-        dataset.addValue(195, series1, "2016-12-24");
-        dataset.addValue(245, series1, "2016-12-25");
+        this.fillHashMap();
+        for(Double d: currentGraphData.keySet()) {
+
+            dataset.addValue(d, series1, currentGraphData.get(d));
+
+        }
 
         return dataset;
     }
 
+    private void fillHashMap() {
+
+        currentGraphData.put(22.0, "01/01/2018");
+        currentGraphData.put(42.0, "06/03/2018");
+        currentGraphData.put(67.0, "09/04/2018");
+        currentGraphData.put(11.0, "14/04/2018");
+        currentGraphData.put(14.0, "18/05/2018");
+        currentGraphData.put(72.0, "26/05/2018");
+        currentGraphData.put(56.0, "01/06/2018");
+        currentGraphData.put(74.0, "03/08/2018");
+        currentGraphData.put(1.0, "22/10/2018");
+        currentGraphData.put(19.0, "04/11/2018");
+
+    }
 
 }
